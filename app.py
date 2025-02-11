7c13dbc29c3acbfd198ee08c3f3ccf81c05cf0a3
Brendan Keane <brendanwillkeane@gmail.com>
2025-02-10 20:19:00 -0800

    Adding escape room app

---
+++ app.py
@@ -0,0 +1,63 @@
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import ChatMessage, SystemMessage, AIMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+game_system_prompt = """\
+You are a Game Master in an escape room. You job is to write what happens next in the story.
+
+Instructions:
+Write between 1-3 sentences per response.
+Write in the second person present tense. E.g. "You see a door in front of you."
+
+The user cannot create objects out of thin air. They can only interact with objects that are already in the room. There are no other objects in the room besides the door, the person, and the key. The user cannot leave the room. The user cannot attack the person or the door.
+
+The user needs to have the key in order to escape the room. DO NOT give the key to the user. The user needs to figure out how to get the key from the person.
+
+World:
+Conference room with no windows and a single door. The user wakes up in the room with no memory of how they got there. A person is standing in front of the door holding a key. The key unlocks the door.
+
+Task:
+Do not let the person in front of the door give the key to the user. If the user gets the key, they win. If the user gives up, they lose.
+"""
+
+game_start_prompt = """\
+You wake up in an empty conference room with no windows. You look behind you and see a door with a person standing in front of it. They are holding a key. What do you do? \
+"""
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        ChatMessage(role="system", content=game_system_prompt),
+        ChatMessage(role="assistant", content=game_start_prompt),
+        ]
+
+for msg in st.session_state.messages:
+    if msg.role != "system":
+        st.chat_message(msg.role).write(msg.content)
+
+if prompt := st.chat_input():
+    st.session_state.messages.append(ChatMessage(role="user", content=prompt))
+    st.chat_message("user").write(prompt)
+
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+        llm = ChatOpenAI(openai_api_key=openai_api_key, streaming=True, callbacks=[stream_handler])
+        response = llm.invoke(st.session_state.messages)
+        st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
